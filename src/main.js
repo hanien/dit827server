3282afa0465b331f71a3fe2a3e01498059e8de33
meliivok <melinda.ivok@gmail.com>
2019-09-30 09:02:29 +0200

    initial website

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import BootstrapVue from 'bootstrap-vue'
+import VueScrollactive from 'vue-scrollactive'
+
+Vue.use(BootstrapVue)
+Vue.use(VueScrollactive)
+
 Vue.config.productionTip = false
 
 new Vue({
