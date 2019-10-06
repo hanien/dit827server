b4fd41c0e9ec843afb609c18db86cf9ae1c5b528
NAzeeh <nazeeh.alhosary@substly.com>
2019-10-06 20:23:53 +0200

    first improve for the api calls

--- server/server.py
+++ server/server.py
@@ -20,19 +20,29 @@ def index():
 @app.route("/api/sensors", methods = ['POST'])
 def post_reading():
     data = request.json
-    collection.insert_one({'type': data['type'], 'value': data['value']})
+    #  collection.insert_one({'type': data['type'], 'value': data['value']})
+    collection.insert_one({ 'temp',{'value': data['value']},'sound',{'value': data['value']} ,'light',{'value': data['value']},'humidity',{'value': data['value']} })
+    # collection.insert_one({'data': {  '$set':{'type': data['type'], 'value': data['value']} } } )
+    # {'type': data['type'], 'value': data['value']},
+    # {'type': data['type'], 'value': data['value']},
+    # {'type': data['type'], 'value': data['value']},
+    # {'type': data['type'], 'value': data['value']},
+    # {'type': data['type'], 'value': data['value']},
+    # {'type': data['type'], 'value': data['value']} })
+    # collection.insert_one({'temp': data['temp'], 'sound': data['sound'], 'light': data['light'], 'humidity': data['humidity']})
     return jsonify(data)
 
 @app.route("/api/sensors/<reading_type>", methods = ['GET'])
 def get_reading(reading_type):
-    found_reading = collection.find_one({'type': reading_type})
+    found_reading = collection.find_one({reading_type})
     return found_reading['value']
 
 @app.route("/api/sensors/<reading_type>", methods = ['PATCH'])
 def update_reading(reading_type):
     data = request.json
-    found_reading = collection.find_one_and_update({'type': reading_type}, {'$set': {'value': data['value']}})
-    return found_reading['value']
+    found_reading = collection.find_one_and_update(reading_type, {'$set': {'value': data[reading_type]}})
+
+    return found_reading[reading_type]
 
 ## START FLASK
 if __name__ == "__main__":
