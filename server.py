15cac2a3d1f1b2e87cb470a831b248a7e2d4c307
hanien <hanienkobus@Haniens-MacBook-Air.local>
2019-12-02 20:33:21 +0100

    changes the website to have carousel and video background

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 ## ROUTES
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("i.html")
 
 """ @app.route("/api/sensors", methods=['POST'])
 def post_reading():
