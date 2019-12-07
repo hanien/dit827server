c9f3e1d2198db58840c7b032a309c1b6fd54d157
hanien <hanienkobus@gmail.com>
2019-12-07 21:54:38 +0100

    adds content in about page. cleans up responsiveness in landing page. adds how-it-works page.

--- server.py
+++ server.py
@@ -4,12 +4,12 @@ import pymongo
 import json
 
 ## SETUP DB CONNECTION AND FLASK ##
-"""connection_string = "mongodb+srv://hanien:hanien123@cluster0-eidux.mongodb.net/test?retryWrites=true&w=majority"
+connection_string = "mongodb+srv://hanien:hanien123@cluster0-eidux.mongodb.net/test?retryWrites=true&w=majority"
 client = pymongo.MongoClient(connection_string)
 db = pymongo.database.Database(client, "aptiv")
-collection = pymongo.collection.Collection(db, "sensorreadings")"""
+collection = pymongo.collection.Collection(db, "sensorreadings")
 app = Flask(__name__)
-#app.config["MONGODB_HOST"] = client
+app.config["MONGODB_HOST"] = client
 
 
 ## ROUTES
@@ -21,14 +21,16 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route("/how-it-works", methods=["GET"])
+def how_it_works():
+    return render_template("how-it-works.html")
+
 @app.route("/api/sensors", methods=['POST'])
 def post_reading():
     data = request.json
     collection.insert_one(
         {'_id':data['_id'], 
         'temperature': data['temperature'],
-        'sound': data['sound'] ,
-        'light': data['light'],
         'humidity':data['humidity'] ,
         'pressure': data['pressure'],
         'altitude': data['altitude'],
@@ -49,14 +51,11 @@ def get_id(rpi_id):
 def update_reading(rpi_id):
     data = json.loads(request.data)
     temperature = data['temperature']
-    #sound = data['sound']
-    #light = data['light']
     humidity = data['humidity']
     pressure = data['pressure']
     altitude = data['altitude']
     gain = data['gain']
     lux = data['lux']
-    #luminosity = data['luminosity']
     ir = data['ir']
     full = data['full']
     found_reading = collection.find_one_and_update(
